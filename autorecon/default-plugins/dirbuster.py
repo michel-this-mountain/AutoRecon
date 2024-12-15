dd797f815e95d5cc636c1a3876a4247ec638d68c
michel <root@Ms-MacBook-Pro.local>
2024-12-15 13:27:57 +0100

    added gobuster as default dirbuster

--- autorecon/default-plugins/dirbuster.py
+++ autorecon/default-plugins/dirbuster.py
@@ -13,7 +13,7 @@ class DirBuster(ServiceScan):
 		self.tags = ['default', 'safe', 'long', 'http']
 
 	def configure(self):
-		self.add_choice_option('tool', default='feroxbuster', choices=['feroxbuster', 'gobuster', 'dirsearch', 'ffuf', 'dirb'], help='The tool to use for directory busting. Default: %(default)s')
+		self.add_choice_option('tool', default='gobuster', choices=['feroxbuster', 'gobuster', 'dirsearch', 'ffuf', 'dirb'], help='The tool to use for directory busting. Default: %(default)s')
 		self.add_list_option('wordlist', default=[os.path.join(config['data_dir'], 'wordlists', 'dirbuster.txt')], help='The wordlist(s) to use when directory busting. Separate multiple wordlists with spaces. Default: %(default)s')
 		self.add_option('threads', default=10, help='The number of threads to use when directory busting. Default: %(default)s')
 		self.add_option('ext', default='txt,html,php,asp,aspx,jsp', help='The extensions you wish to fuzz (no dot, comma separated). Default: %(default)s')
